2410e90ba1e165f86666d8021fec6f643d4fe61f
akani mbambo <Akanimbambo71@gmail.com>
2026-05-17 17:32:39 +0200

    New changes to the visual part of the code, image has been chaneged and background color has been changed to black

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,77 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+  
 namespace WPF_Chatbot
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ // Start of namspace
     public partial class MainWindow : Window
     {
+        //start of class
         public MainWindow()
         {
+            //start of constructor
+
             InitializeComponent();
+
+            //end of constructor
         }
+
+        private void start_Chatbot (object sender, RoutedEventArgs e)
+        {
+            // start of start chatboot method
+
+            //set the logo page to hidden
+            logo_grid.Visibility = Visibility.Hidden;
+
+            //set username grid to visable
+            username_grid.Visibility = Visibility.Visible;
+
+            //end of start chatbot method
+        }
+
+        private void submit_names(object sender, RoutedEventArgs e)
+        {
+            // start of submit name method
+
+            //get the username from the text box
+            string username = user_name.Text.ToString();
+
+            //check is username is empty
+            if(username != "")
+            {
+                //start of if
+
+                //show message
+                MessageBox.Show(" wellcome " + user_name);
+
+                //set the username grid hidden and chats visable
+                username_grid.Visibility = Visibility.Hidden;
+                chats_grid.Visibility = Visibility.Visible;
+
+                //add the message to the chats list
+                chats_list.Items.Add("ChatBot: hello " + user_name);
+
+                //end of if
+            }
+            else
+            {
+                // start of else
+                MessageBox.Show("Please enter a username");
+            }
+
+        }
+        private void send_question(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+
+
+
+        //end of class
     }
+
+    // End of namespace
 }
