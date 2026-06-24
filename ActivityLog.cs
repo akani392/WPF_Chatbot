c17bfd6b20a8c87a2534b01982979983212aacb9
akani mbambo <Akanimbambo71@gmail.com>
2026-06-24 15:37:20 +0200

    Completed task 3 of the POE just put in activityLog class it keeps a memroy list of the chats with the chatbot

--- ActivityLog.cs
+++ ActivityLog.cs
@@ -1,6 +1,62 @@
-﻿namespace WPF_Chatbot
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Chatbot
 {
-    internal class ActivityLog
+    // Start of namespace
+    public class ActivityLog
     {
+        // Start of class
+
+        // Each entry stores the action text and the time it happened
+        class LogEntry
+        {
+            // Start of log entry class
+            public string Action { get; set; }
+            public DateTime Time { get; set; }
+            // End of log entry class
+        }
+
+        // The list of all actions taken this session
+        List<LogEntry> entries = new List<LogEntry>();
+
+        // Adds a new action to the log
+        public void AddEntry(string action)
+        {
+            // Start of add entry method
+            entries.Add(new LogEntry
+            {
+                Action = action,
+                Time = DateTime.Now
+            });
+            // End of add entry method
+        }
+
+        // Returns a numbered summary of all actions taken, newest last
+        public string GetSummary()
+        {
+            // Start of get summary method
+            if (entries.Count == 0)
+                return "No actions have been recorded yet this session.";
+
+            StringBuilder sb = new StringBuilder("Here's a summary of recent actions:\n\n");
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                sb.Append(i + 1)
+                  .Append(". ")
+                  .Append(entries[i].Action)
+                  .Append("  (")
+                  .Append(entries[i].Time.ToString("HH:mm"))
+                  .Append(")\n");
+            }
+
+            return sb.ToString().TrimEnd();
+            // End of get summary method
+        }
+
+        // End of class
     }
+    // End of namespace
 }
